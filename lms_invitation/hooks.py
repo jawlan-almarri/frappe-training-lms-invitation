634dddb44a6f58655ded0f4928e5efca8e8a38f4
Jawlan Almarri <Jawlan.almarri@gmail.com>
2026-06-09 14:58:24 +0300

    Convert LMS Invitation doctypes to standard

--- lms_invitation/hooks.py
+++ lms_invitation/hooks.py
@@ -256,19 +256,8 @@ app_license = "mit"
 # List of apps whose translatable strings should be excluded from this app's translations.
 # ignore_translatable_strings_from = []
 
+
 fixtures = [
-    {
-        "dt": "DocType",
-        "filters": [
-            ["name", "=", "Invitation Code"]
-        ],
-    },
-    {
-        "dt": "Module Def",
-        "filters": [
-            ["name", "=", "LMS Invitation"]
-        ],
-    },
     {
         "dt": "Custom Field",
         "filters": [
